d89fbb51d1642df7690b3f68cade4e4f24613217
Luis <luisfru96@gmail.com>
2021-09-21 20:52:56 +0200

    handleRemoveElement-handleAddCompleted

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,7 +12,7 @@ const Todo = ({ handleRemoveElement, handleAddCompleted, text }) => {
 };
 
 Todo.propTypes = {
-  handleRemove: PropTypes.func,
+  handleRemoveElement: PropTypes.func,
   handleAddCompleted: PropTypes.func,
   text: PropTypes.string,
 };
